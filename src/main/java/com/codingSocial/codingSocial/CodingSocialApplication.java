13849d7d60f47751928fb183cc5d172b3ecab621
Ashreet <ashreet14@gmail.com>
2023-07-07 11:53:38 -0400

    Initialize project with Sping

---
+++ src/main/java/com/codingSocial/codingSocial/CodingSocialApplication.java
@@ -0,0 +1,13 @@
+package com.codingSocial.codingSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingSocialApplication.class, args);
+	}
+
+}
