78347440fb7f6794d03b9a5d895ae1f9f973d22f
Atheesh Thanaseelan <atheesh.k29@gmail.com>
2023-07-08 13:05:16 -0400

    Add Register Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import Login from "./Pages/LoginPage/Login";
+import Register from "./Pages/RegisterPage/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" exact component={Home} /> */}
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/register" element={<Register/>  } />
+      </Routes>
+    </Router>
   );
 }
 
