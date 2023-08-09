c855ee11a8a6d44665c4e70be1e1d14e7989de09
Atheesh Thanaseelan <atheesh.k29@gmail.com>
2023-08-09 13:22:59 -0400

    update frontend to begin authentication implementation

--- frontend/src/Pages/LoginPage/Login.js
+++ frontend/src/Pages/LoginPage/Login.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import LoginForm from '../../Components/LoginForm';
+import { checkLoginStatus } from '../../common';
+import { logoutFunction } from '../../common';
+
+const handleLogout = async (event) => {
+  event.preventDefault();
+  logoutFunction();
+  window.location.reload(); 
+};
+
+function LoginContent(){
+  if (checkLoginStatus()){
+    return <form onSubmit={handleLogout}><button type="submit">log out {sessionStorage.getItem("login")}</button></form>
+  }else{
+    return <LoginForm/>
+  }
+}
 
 function Login() {
+
+  console.log(checkLoginStatus());
   return (
     <div className="login-container">
       <h1>hello from LOGIN</h1>
-      <LoginForm/>
+      <LoginContent/>
     </div>
   );
-}
+} 
 
 export default Login;
